c262492893c4b687428be204534dc9a1ba5d364a
ivantus1988 <ivantus1988@gmail.com>
2020-11-21 17:30:47 +0300

    add commit

--- src/Task2.java
+++ src/Task2.java
@@ -1,14 +1,23 @@
 public class Task2 {
     public static void main(String[] args) {
-        int n=3;
-        int inStart = 5;
-        int [][] array = new int[1][3];
-        for (int i=0; i< array.length; i++) {
-            for (int j = 0; j < array[i].length - 1; j++) {
-
-                System.out.print(array[i][j]);
-                System.out.println();
+        int inStart;
+        inStart=5;
+        int [][] array = new int[3][];
+        for(int i=0; i<array.length; i++) {
+            array[i] = new int[i + 1];
+        }
+        for (int i=0; i<array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                inStart+=inStart;
+                array[i][j] =inStart/2;
+                System.out.print(array[i][j] + " ");
             }
+            System.out.println();
+        }
+        System.out.println();
+        int inStart2 = 5;
+        for(int i=0; i<array.length; i++) {
+            array[i] = new int[i + 1];
         }
     }
 }
--- src/Task3.java
+++ src/Task3.java
@@ -6,16 +6,19 @@ public class Task3 {
             System.out.print(array[i] + " ");
         }
         System.out.println();
+        System.out.print("Элементы, которые делятся на 3 и на 7: ");
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 3 == 0 && array[i] % 7 == 0)
                 System.out.print(array[i] + "  ");
         }
         System.out.println();
+        System.out.print("Элементы, которые делятся на 5 или на 7, но не делятся на 35: ");
         for (int i = 0; i < array.length; i++) {
             if ((array[i] % 5 == 0 || array[i] % 7 == 0) && array[i] % 35 != 0)
                 System.out.print(array[i] + " ");
         }
         System.out.println();
+        System.out.print("Элементы, диапазонов [10; 19], [30; 39], [50; 59], [70; 79], [90; 99], которые делятся на 5 или на 7, но не делятся на 35: ");
         for (int i = 0; i < array.length; i++) {
             if ((((array[i] <= 19 && array[i] >= 10) || (array[i] <= 39 && array[i] >= 30) || (array[i] <= 59 && array[i] >= 50) || (array[i] <= 79 && array[i] >= 70) || (array[i] <= 99 && array[i] >= 90)) && (array[i] % 5 == 0 || array[i] % 7 == 0)) && (array[i] % 35 != 0))
                 System.out.print(array[i] + " ");
