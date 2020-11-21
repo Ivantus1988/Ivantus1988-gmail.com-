8f7f7a4de63c6b64623e15dd983ac5d13407e52e
ivantus1988 <ivantus1988@gmail.com>
2020-11-21 18:29:21 +0300

    add commit

--- src/Task2.java
+++ src/Task2.java
@@ -1,23 +1,19 @@
 public class Task2 {
     public static void main(String[] args) {
         int inStart;
-        inStart=5;
-        int [][] array = new int[3][];
-        for(int i=0; i<array.length; i++) {
+        inStart = 5;
+        int[][] array = new int[3][];
+        for (int i = 0; i < array.length; i++) {
             array[i] = new int[i + 1];
         }
-        for (int i=0; i<array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                inStart+=inStart;
-                array[i][j] =inStart/2;
+                inStart += inStart;
+                array[i][j] = inStart / 2;
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println();
-        int inStart2 = 5;
-        for(int i=0; i<array.length; i++) {
-            array[i] = new int[i + 1];
         }
     }
-}
+
